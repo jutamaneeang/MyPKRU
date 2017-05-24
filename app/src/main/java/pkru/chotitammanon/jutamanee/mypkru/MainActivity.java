b5b508c5130e40b2e297a4d5d0a66ac89e0db0a5
testapp <janenanaka@gmail.com>
2017-05-24 13:39:42 +0700

    finsh23may

--- app/src/main/java/pkru/chotitammanon/jutamanee/mypkru/MainActivity.java
+++ app/src/main/java/pkru/chotitammanon/jutamanee/mypkru/MainActivity.java
@@ -1,13 +1,58 @@
 package pkru.chotitammanon.jutamanee.mypkru;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //Explicit
+    private EditText userEditText, passwordEditText;
+    private TextView textView;
+    private Button button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //initial View
+        initialView();
+
+        //controller
+        controller();
+
+
+    }   //Main Method
+
+    private void controller() {
+        textView.setOnClickListener(this);
+        button.setOnClickListener(this);
     }
-}
+
+    private void initialView() {
+        userEditText = (EditText) findViewById(R.id.edtUser);
+        passwordEditText = (EditText) findViewById(R.id.edtPassword);
+        textView = (TextView) findViewById(R.id.txtNewRegister);
+        button = (Button) findViewById(R.id.btnLogin);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        //For TextView
+        if (v == textView) {
+            //intent to NewRegister
+            Intent intent = new Intent(MainActivity.this, NewRegisterActivity.class);
+            startActivity(intent);
+        }
+        //for button
+        if (v == button) {
+        }
+    }
+}   //main class
